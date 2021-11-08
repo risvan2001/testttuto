7230e92f569eeaffb3f8f6bb97337391b964a16f
Risvan Suryansyah <risvan682@gmail.com>
2021-11-08 13:47:54 +0700

    change main.java

--- src/com/companyzRs/Main.java
+++ src/com/companyzRs/Main.java
@@ -3,6 +3,6 @@ package com.companyzRs;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("The Number i : " + new  RandomNumberGen().getRandomNumber());
+	System.out.println("The Number i : " + new  RandomNumberGen().getRandomNumber(99));
     }
 }
